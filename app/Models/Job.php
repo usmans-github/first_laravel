5c523baab8a6a184ebdaecd6002298bcdbd231b7
M Usman <usmanali730771@gmail.com>
2024-12-30 22:10:06 +0500

    learn tinker migration models overview

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -3,43 +3,9 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Arr;
 
-class Job
-{
-    public static function all(): array
-    {
-        return [
-            [
-                "id" => 1,
-                "title" => "Mern Stack Developer",
-                "salary" => "$50,000"
-            ],
-            [
-                "id" => 2,
-                "title" => "Mean Stack Developer",
-                "salary" => "$40,000"
-            ],
-            [
-                "id" => 3,
-                "title" => "Java Developer",
-                "salary" => "$60,000"
-            ],
-            [
-                "id" => 4,
-                "title" => "Backend Developer",
-                "salary" => "$65,000"
-            ],
-        ];
-    }
 
-    public static function find(int $id): array 
-    {
-        return   Arr::first(static::all(), fn($job) => $job["id"] === $id);
-
-        if(! $job){
-            abort(404);
-        }
-        return $job;
-    }
-}
+class Job extends Model {
+    protected $table = 'joblistings';
+    protected $fillable = ['title', 'salary'];
+}       
--- routes/web.php
+++ routes/web.php
@@ -8,8 +8,15 @@ use App\Models\Job;
 
 
 Route::get('/', function () {
+  
     return view('home');
 });
+
+
+// Route::get('/', function () {
+//   $job = Job::all();
+//   dd($job[0]->title);
+// });
 
 Route::get("/jobs", function () {
     return view("jobs", [
