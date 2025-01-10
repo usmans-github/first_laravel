d56eb707650dcd1f5124fede212263fcb9177603
usmans-github <usmanali730771@gmail.com>
2025-01-10 20:19:54 +0500

    fresh the database

--- app/Models/Employer.php
+++ app/Models/Employer.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Employer extends Model
 {
     use HasFactory;
-    public function jobs()
+    public function jobs()          
     {
         return $this->hasMany(Job::class);
     }
